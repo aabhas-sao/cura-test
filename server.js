8a20f09b1bc58125f40e922e9b1ff97652f6dbf9
= <=>
2022-02-13 23:22:00 +0530

    poem: comment res.download
    Signed-off-by: = <=>

--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.get("/", (req, res) => {
 app.post("/slice", upload.single("uploaded_file"), (req, res) => {
   console.log(req.file);
   sliceModel(req.file.filename);
-  res.download(`outputs/${req.file.filename.split(".")[0]}.gcode`);
+  // res.download(`${appDir}outputs/${req.file.filename.split(".")[0]}.gcode`);
+  res.send(200);
 });
 
 app.listen(PORT, () => {
